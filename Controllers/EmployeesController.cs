f9f6190bcb1d70037abd20f887496457cbae6ef4
Narahari Nallagatla <nallagatlanarahari@gmail.com>
2026-05-18 12:14:16 +0530

    Initial Azure API project

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using EmployeeApi.Data;
+using EmployeeApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Employee>>> GetEmployees()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> AddEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(employee);
+        }
+    }
+}
